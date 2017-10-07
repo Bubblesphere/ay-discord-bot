cd31ce3a9e54fc2beccb1f1cfc646df62742a3c3
Bruno Roy <brusi_roy@hotmail.com>
2017-10-07 11:32:23 -0400

    Code formatting
    Used YAPF to format the code
    CAPITALIZED all the constants

--- bot.py
+++ bot.py
@@ -1,52 +1,62 @@
-import praw
-import discord
-from discord.ext import commands
+""" Discord Bot """
 
 import os
+import praw
+from discord.ext import commands
+
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
-reddit_object = praw.Reddit(client_id = os.environ.get("REDDIT_CLIENT_ID"),
-                     client_secret = os.environ.get("REDDIT_CLIENT_SECRET"),
-                     user_agent = "aySH Bot")
+REDDIT = praw.Reddit(
+    client_id=os.environ.get("REDDIT_CLIENT_ID"),
+    client_secret=os.environ.get("REDDIT_CLIENT_SECRET"),
+    user_agent="aySH Bot")
 
 # Verify that we are in `read_only` mode
-print(reddit_object.read_only)
+print(REDDIT.read_only)
+
 
 async def top_subreddit(subreddit, time):
-    tops = reddit_object.subreddit(subreddit).top(time, limit = 1)
+    tops = REDDIT.subreddit(subreddit).top(time, limit=1)
     for top in tops:
-        await client.say(top.url)
+        await CLIENT.say(top.url)
+
 
 # Bot config
-prefix = '!'
-des = 'aySH'
-client = commands.Bot(description=des, command_prefix=prefix)
+PREFIX = '!'
+DESC = 'aySH'
+CLIENT = commands.Bot(description=DESC, command_prefix=PREFIX)
+
 
 # Make a startup command
-@client.event
+@CLIENT.event
 async def on_ready():
     print("[*]I'm in")
-    print('[*] Name: {}'.format(client.user.name))
+    print('[*] Name: {}'.format(CLIENT.user.name))
+
 
 # subreddit
-@client.command(pass_context=True)
-async def reddit(ctx, subreddit = "all", time = "day"):
+@CLIENT.command(pass_context=True)
+async def reddit(ctx, subreddit="all", time="day"):
     await top_subreddit(subreddit, time)
 
+
 # random
-@client.command(pass_context=True)
+@CLIENT.command(pass_context=True)
 async def random(ctx):
     await top_subreddit('random', 'all')
 
+
 # probuild
-@client.command(pass_context=True)
-async def build(ctx, champion = "janna"):
-    await client.say('http://www.probuilds.net/champions/details/' + champion)
+@CLIENT.command(pass_context=True)
+async def build(ctx, champion="janna"):
+    await CLIENT.say('http://www.probuilds.net/champions/details/' + champion)
+
 
 # counter
-@client.command(pass_context=True)
-async def counter(ctx, champion = "janna"):
-    await client.say('http://lolcounter.com/champions/' + champion)
+@CLIENT.command(pass_context=True)
+async def counter(ctx, champion="janna"):
+    await CLIENT.say('http://lolcounter.com/champions/' + champion)
 
-client.run(os.environ.get("DISCORD_CLIENT_TOKEN"))
+
+CLIENT.run(os.environ.get("DISCORD_CLIENT_TOKEN"))
